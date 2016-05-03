554f2f6368f0bcf9c4f1da051b0325729194c8e4
Graham Robinson <graham@yelcho.com.au>
2016-05-03 15:41:46 +1000

    Step 17 Complete

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -13,6 +13,12 @@ sap.ui.define([
 				// telling what resource the user tried to access...
 				jQuery.sap.log.error("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
 			});
+			oRouter.attachRouteMatched(function (oEvent){
+				var sRouteName = oEvent.getParameter("name");
+				// do something, i.e. send usage statistics to backend
+				// in order to improve our app and the user experience (Build-Measure-Learn cycle)
+				jQuery.sap.log.warning("User accessed route " + sRouteName + ", timestamp = " + new Date().getTime());
+			});
 		}
 
 	});
