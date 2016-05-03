e14e8367f02902f8facc99ce0b78f68e4b7426db
Graham Robinson <graham@yelcho.com.au>
2016-05-03 14:10:36 +1000

    Step 5 Complete

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,10 +1,15 @@
 sap.ui.define([
-   "ui5/workshop/controller/BaseController"
-], function (BaseController) {
-   "use strict";
+    "ui5/workshop/controller/BaseController"
+], function(BaseController) {
+    "use strict";
 
-   return BaseController.extend("ui5.workshop.controller.Home", {
-
-   });
+    return BaseController.extend("ui5.workshop.controller.Home", {
+        onDisplayNotFound: function(oEvent) {
+            //display the "notFound" target without changing the hash
+            this.getRouter().getTargets().display("notFound", {
+            	fromTarget : "home"
+            });
+        }
+    });
 
 });
