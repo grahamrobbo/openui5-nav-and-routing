72b0090e31dd4364b39d92a733b8c75584fb6d07
Graham Robinson <graham@yelcho.com.au>
2016-05-03 14:37:41 +1000

    Step 9 Complete

--- webapp/controller/employees/Resume.controller.js
+++ webapp/controller/employees/Resume.controller.js
@@ -1,14 +1,18 @@
 sap.ui.define([
-	"ui5/workshop/controller/BaseController"
-], function (BaseController) {
+	"ui5/workshop/controller/BaseController",
+	"sap/ui/model/json/JSONModel"
+], function (BaseController, JSONModel) {
 	"use strict";
+	var _aValidTabKeys = ["Info", "Projects", "Hobbies", "Notes"];
 	return BaseController.extend("ui5.workshop.controller.employees.Resume", {
 		onInit: function () {
 			var oRouter = this.getRouter();
+			this.getView().setModel(new JSONModel(), "view");
+
 			oRouter.getRoute("employeeResume").attachMatched(this._onRouteMatched, this);
 		},
 		_onRouteMatched : function (oEvent) {
-			var oArgs, oView;
+			var oArgs, oView, oQuery;
 			oArgs = oEvent.getParameter("arguments");
 			oView = this.getView();
 			oView.bindElement({
@@ -23,12 +27,38 @@ sap.ui.define([
 					}
 				}
 			});
+			oQuery = oArgs["?query"];
+			if (oQuery && _aValidTabKeys.indexOf(oQuery.tab) > -1){
+				oView.getModel("view").setProperty("/selectedTabKey", oQuery.tab);
+			} else {
+				// the default query param should be visible at all time
+				this.getRouter().navTo("employeeResume", {
+					employeeId : oArgs.employeeId,
+					query: {
+						tab : _aValidTabKeys[0]
+					}
+				},true /*no history*/);
+			}
 		},
 		_onBindingChange : function (oEvent) {
 			// No data for the binding
 			if (!this.getView().getBindingContext()) {
 				this.getRouter().getTargets().display("notFound");
 			}
-		}
+		},
+		/**
+		 * We use this event handler to update the hash in case a new tab is selected.
+		 * @param oEvent
+
+		 */
+		onTabSelect : function (oEvent){
+			var oCtx = this.getView().getBindingContext();
+			this.getRouter().navTo("employeeResume", {
+				employeeId : oCtx.getProperty("EmployeeID"),
+				query: {
+					tab : oEvent.getParameter("selectedKey")
+				}
+			}, true /*without history*/);
+		}		
 	});
 });
