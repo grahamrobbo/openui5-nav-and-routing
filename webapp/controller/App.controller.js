868bdb12dd2b008d108f9c5224bd5f7ce41dab04
Graham Robinson <graham@yelcho.com.au>
2016-05-03 15:28:55 +1000

    Step 16 Complete

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,7 +6,13 @@ sap.ui.define([
 	return BaseController.extend("ui5.workshop.controller.App", {
 
 		onInit: function () {
-
+			var oRouter = this.getRouter();
+			oRouter.attachBypassed(function (oEvent) {
+				var sHash = oEvent.getParameter("hash");
+				// do something here, i.e. send logging data to the backend for analysis
+				// telling what resource the user tried to access...
+				jQuery.sap.log.info("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
+			});
 		}
 
 	});
